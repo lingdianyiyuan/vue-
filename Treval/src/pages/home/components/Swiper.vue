c9f57fc1a9e8fd31e024892e8994cb201e20d10f
yaoyang <yaoyang9527@aliyun.com>
2019-08-08 17:59:16 +0800

    swiper

---
+++ Treval/src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="swiper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-banner" :src="item.imgURL" alt="">
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgURL: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg'
+        },
+        {
+          id: '002',
+          imgURL: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
+        },
+        {
+          id: '003',
+          imgURL: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/0b0622e5b6ac5fbed3084dd2832e6f8f.jpg_750x200_895321b6.jpg'
+        }
+
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .swiper >>> .swiper-pagination-bullet
+        background #ffffff
+    .swiper
+        overflow hidden
+        width :100%
+        height :0
+        padding-bottom :26.5%
+        .swiper-banner
+            width :100%;
+</style>
