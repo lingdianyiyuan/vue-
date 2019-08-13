bdf7eea621e3a9e9b191073e1f3ec041452c4708
yaoyang <yaoyang9527@aliyun.com>
2019-08-13 16:16:12 +0800

    weekend组件

--- Treval/src/pages/home/components/Recommend.vue
+++ Treval/src/pages/home/components/Recommend.vue
@@ -1,118 +1,112 @@
 <template>
-  <div class="recommend-content">
-    <div class="recommend-title">
+  <div class='recommend-content'>
+    <div class='recommend-title'>
       <img
-        class="recommend-img"
-        src="http://img1.qunarzz.com/piao/fusion/1711/16/bfbb9874e8f11402.png"
+        class='recommend-img'
+        src='http://img1.qunarzz.com/piao/fusion/1711/16/bfbb9874e8f11402.png'
         alt
       />
       本周热门榜单
-      <div class="popularList">
+      <div class='popularList'>
         全部榜单
-        <span class="iconfont">&#xe612;</span>
+        <span class='iconfont'>&#xe612;</span>
       </div>
     </div>
-    <div class="recommend-list">
-      <swiper :options="swiperOption-recommend">
+    <div class='recommend-list'>
+      <swiper :options='swiperOption'>
         <!-- slides -->
-        <swiper-slide v-for="item of RecommendList" :key="item.id">
-          <img class="popularList-img" :src="item.imgURL" alt />
-          <p class="popularList-title">{{item.title}}</p>
-          <p class="popularList-price">{{item.price}}</p>
+        <swiper-slide v-for='item of RecommendList' :key='item.id'>
+          <img class='popularList-img' :src='item.imgURL' alt />
+          <p class='popularList-title'>{{item.title}}</p>
+          <p class='popularList-price'><span class="priceColor">￥{{item.price}}</span>起</p>
         </swiper-slide>
         <!-- Optional controls -->
-        <!-- <div class="swiper-pagination" slot="pagination"></div> -->
+        <!-- <div class='swiper-pagination' slot='pagination'></div> -->
       </swiper>
-      
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HomeRecommend",
-  data() {
+  name: 'HomeRecommend',
+  data () {
     return {
-    swiperOption: {
-        pagination: '.swiperOption-recommend',
-        slidesPerView : 3,
-        centeredSlides : true,
+      swiperOption: {
+        pagination: '.swiperOption',
+        cssWidthAndHeight: false,
+        slidesPerView: 3.7,
+        offsetSlidesAfter: 2,
+        centeredSlides: false
       },
       RecommendList: [
         {
-          id: "001",
-          imgURL:
-            "http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg",
-          title: "故宫",
-          price: "￥60"
+          id: '001',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_250x250_0fc722c0.jpg',
+          title: '故宫',
+          price: 60
         },
         {
-          id: "002",
-          imgURL:
-            "http://img1.qunarzz.com/sight/p0/1708/2b/2b3b94de99c0a425a3.img.jpg_250x250_50c99cf3.jpg",
-          title: "八达岭长城",
-          price: "￥40"
+          id: '002',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/1708/2b/2b3b94de99c0a425a3.img.jpg_250x250_50c99cf3.jpg',
+          title: '八达岭长城',
+          price: 40
         },
         {
-          id: "003",
-          imgURL:
-            "http://img1.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_250x250_63e3520a.jpg",
-          title: "圆明园",
-          price: "￥25"
+          id: '003',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_250x250_63e3520a.jpg',
+          title: '圆明园',
+          price: 25
         },
         {
-          id: "004",
-          imgURL:
-            "http://img1.qunarzz.com/sight/p0/1505/d2/d274c92de14c93da.water.jpg_250x250_d852635b.jpg",
-          title: "颐和园",
-          price: "￥38"
+          id: '004',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/1505/d2/d274c92de14c93da.water.jpg_250x250_d852635b.jpg',
+          title: '颐和园',
+          price: 38
         },
         {
-          id: "005",
-          imgURL:
-            "http://img1.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_250x250_6ec2f798.jpg",
-          title: "恭王府",
-          price: "￥27"
+          id: '005',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_250x250_6ec2f798.jpg',
+          title: '恭王府',
+          price: 27
         },
         {
-          id: "006",
-          imgURL:
-            "http://img1.qunarzz.com/sight/p0/1902/84/84696f368bbec10da3.img.jpg_250x250_3588bc9a.jpg",
-          title: "北京世界园艺博览会",
-          price: "￥108"
+          id: '006',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/1902/84/84696f368bbec10da3.img.jpg_250x250_3588bc9a.jpg',
+          title: '北京世界园艺博览会',
+          price: 108
         },
         {
-          id: "007",
-          imgURL:
-            "http://img1.qunarzz.com/sight/p0/1501/40/40b2b6c951b28fdd.water.jpg_250x250_ecfa0df2.jpg",
-          title: "水立方",
-          price: "￥246"
+          id: '007',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/1501/40/40b2b6c951b28fdd.water.jpg_250x250_ecfa0df2.jpg',
+          title: '水立方',
+          price: 246
         },
         {
-          id: "008",
-          imgURL:
-            "http://img1.qunarzz.com/sight/p0/1602/92/920e47352552c1c990.water.jpg_250x250_ee99c18e.jpg",
-          title: "天坛公园",
-          price: "￥40"
+          id: '008',
+          imgURL: 'http://img1.qunarzz.com/sight/p0/1602/92/920e47352552c1c990.water.jpg_250x250_ee99c18e.jpg',
+          title: '天坛公园',
+          price: 40
         }
       ]
-    };
+    }
   }
-};
+}
 </script>
 
-<style lang="stylus" scoped>
+<style lang='stylus' scoped>
+@import '~styles/minins.styl'
 .recommend-content >>> .swiper-slide
     width :2rem !important;
     margin :.1rem;
-.recommend-content 
+.recommend-content
     margin-top: 0.2rem;
-    .recommend-title 
+    .recommend-title
         height: 0.44rem;
         padding: 0 0.1rem;
         line-height: 0.44rem;
         font-size: 0.32rem;
-        .recommend-img 
+        .recommend-img
             width: 0.3rem;
             height: 0.3rem;
             vertical-align: top;
@@ -127,12 +121,12 @@ export default {
         box-sizing :border-box;
         padding :.15rem 0;
         text-align :center;
+        color :#212121;
+        ellipsis()
     .popularList-price
         box-sizing :border-box;
-        padding :.15rem 0;
         text-align :center;
-
-        
-  
-
+        color :#212121;
+        .priceColor
+          color :#ff8300;
 </style>
---
+++ Treval/src/pages/home/components/Weekend.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="weekend">
+        <div class="weekend-title">
+            周末去哪儿
+        </div>
+        <div class="weekend-content border-bottom">
+            <ul>
+                <li v-for="item of WeekendList" :key="item.id">
+                    <img class="weekend-content-img" :src="item.imgURL" alt="">
+                    <p class="weekend-add">{{item.add}}</p>
+                    <p class="weekend-description">{{item.description}}</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      WeekendList: [
+        {
+          id: '001',
+          imgURL: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+          add: '京城周末撒欢',
+          description: '在帝都过周末，不仅仅是城中游'
+        },
+        {
+          id: '002',
+          imgURL: 'http://img1.qunarzz.com/sight/source/1811/7e/476589267ebb41.jpg_r_640x214_bf599709.jpg',
+          add: '京城溜娃必去',
+          description: '德智体美劳全面发展的亲子日，这些地方该去看看…'
+        },
+        {
+          id: '003',
+          imgURL: 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+          add: '帝都必打卡',
+          description: '来北京必去的景点非这些地方莫属'
+        },
+        {
+          id: '004',
+          imgURL: 'http://img1.qunarzz.com/sight/source/1606/85/c6b6bad1b977e.jpg_r_640x214_5c638941.jpg',
+          add: '漂流勇进，烧烤露营',
+          description: '轻松水上漂，任我乐逍遥'
+        },
+        {
+          id: '005',
+          imgURL: 'http://img1.qunarzz.com/sight/source/1505/53/02091f1ee43d0a.jpg_r_640x214_2e3256b1.jpg',
+          add: '北京清凉避暑胜地',
+          description: '到大自然中享受“天然空调”的清凉'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/minins.styl';
+    .weekend
+        margin-top :.1rem;
+        .weekend-title
+            height :.8rem;
+            line-height :.8rem;
+            background :#eee;
+            padding-left :.26rem;
+        .weekend-content
+            height :4rem;
+            padding-bottom :.2rem;
+            .weekend-content-img
+                width :100%;
+                height :3rem;
+            .weekend-add
+                font-size :.28rem;
+                line-height :.44rem;
+                padding :.2rem 1.4rem 0 .2rem;
+                ellipsis()
+            .weekend-description
+                font-size :.24rem;
+                line-height :.44rem;
+                padding-right :1.4rem;
+                padding-left :.2rem;
+                color #616161;
+                ellipsis()
+</style>
